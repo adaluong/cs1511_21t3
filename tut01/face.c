a47f51614f6dd5c5a9ae7bb8a6a30c4e55d12398
Ada Luong <ada.luong@student.unsw.edu.au>
2021-09-15 10:36:14 +1000

    added tut01 content

---
+++ tut01/face.c
@@ -0,0 +1,16 @@
+// COMP1511 Tut01 - demonstrating escape sequences
+// Printing a face with double backslashes
+// Ada Luong, September 2021
+
+# include <stdio.h>
+
+int main (void) {
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\_/\n");
+    
+    return 0;
+
+}
---
+++ tut01/hello_world.c
@@ -0,0 +1,12 @@
+// COMP1511 Tut01 - Hello World!
+// Ada Luong, September 2021
+
+# include <stdio.h>
+
+int main (void) {
+
+    printf("Hello, world!\n");
+    
+    return 0;
+
+}
